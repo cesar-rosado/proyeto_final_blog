d4a06858554b9b33b13ca31546b261a4f66bb621
César Ismael Rosado Villanueva <cirv84@hotmail.com>
2023-05-28 15:21:23 -0500

    Agregando la vista de incio y cerrar sesion, urls y templates

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from usuarios.views import registro
+from usuarios.views import registro, login_view, CustomLogoutView
 
 urlpatterns = [
     path('registro/', registro, name="registro"),
+    path('login/', login_view, name="login"),
+    path('logout/', CustomLogoutView.as_view(), name="logout"),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LogoutView, LoginView
+
 from usuarios.forms import RegisterForm
 # Create your views here.
 
@@ -19,3 +23,33 @@ def registro(request):
         template_name='usuarios/registro.html',
         context={'form': formulario},
     )
+
+def login_view(request):
+    next_url = request.GET.get('next')
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            usuario = data.get('username')
+            password = data.get('password')
+            user = authenticate(username=usuario, password=password)
+            # user puede ser un usuario o None
+            if user:
+                login(request=request, user=user)
+                if next_url:
+                    return redirect(next_url)
+                url_exitosa = reverse('inicio')
+                return redirect(url_exitosa)
+    else:  # GET
+        form = AuthenticationForm()
+    return render(
+        request=request,
+        template_name='usuarios/login.html',
+        context={'form': form},
+    )
+
+
+class CustomLogoutView(LogoutView):
+   template_name = 'usuarios/logout.html'
+
