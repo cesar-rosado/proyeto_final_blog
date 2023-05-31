ac04df5f5fac13958c8b7842048d12255f0059a3
César Ismael Rosado Villanueva <cirv84@hotmail.com>
2023-05-31 15:10:13 -0500

    Agregando modificaciones para visualizar el avatar del usuario

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Avatar(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    imagen = models.ImageField(upload_to='avatares', null=True, blank=True)
+    imagen = models.ImageField(upload_to='avatares', null=True, blank=True,)
 
     def __str__(self):
         return f"Avatar de: {self.user}"
